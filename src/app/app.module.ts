1146785c0076dd51004d9bce612363539cd6774c
unscrud <danieldevop@gmail.com>
2024-08-28 14:23:03 +0000

    Criação do card depoimento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatCardModule} from '@angular/material/card';
 import { ContainerComponent } from './shared/container/container.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CardBuscaComponent } from './shared/card-busca/card-busca.component';
+import { CardDepoimentoComponent } from './shared/card-depoimento/card-depoimento.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { CardBuscaComponent } from './shared/card-busca/card-busca.component';
     CardComponent,
     ContainerComponent,
     HomeComponent,
-    CardBuscaComponent
+    CardBuscaComponent,
+    CardDepoimentoComponent
   ],
   imports: [
     BrowserModule,
