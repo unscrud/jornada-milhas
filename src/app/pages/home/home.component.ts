7827d4aa5d78b21376fe8cf08114715ef990a8c2
unscrud <danieldevop@gmail.com>
2024-09-09 22:52:38 +0000

    Removendo o teste

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { PromocaoService } from 'src/app/core/services/promocao.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-  constructor( private servicoPromocao: PromocaoService){}
-
-  ngOnInit(): void {
-    this.servicoPromocao.listar()
-        .subscribe( resposta => {
-          console.log(resposta)
-        })
-  }
-}
+export class HomeComponent {}
