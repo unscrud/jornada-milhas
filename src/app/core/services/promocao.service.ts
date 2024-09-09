f8f438e1aa570b17c394f269c20f03b7a3082eec
unscrud <danieldevop@gmail.com>
2024-09-09 22:46:49 +0000

    Buscando a lista de promoções da api

--- src/app/core/services/promocao.service.ts
+++ src/app/core/services/promocao.service.ts
@@ -2,17 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Promocao } from '../types/promocao';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PromocaoService {
 
+  private apiUrl: string = environment.apiUrl
+
   constructor(
     private httpClient: HttpClient
   ) { }
 
   listar(): Observable<Promocao[]>{
-    return this.httpClient.get<Promocao[]>("")
+    return this.httpClient.get<Promocao[]>(`${this.apiUrl}/promocao`)
   }
 }
