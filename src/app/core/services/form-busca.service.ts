d0cb30d29b7a069c257b9fb111333338286640cf
unscrud <danieldevop@gmail.com>
2024-09-12 21:00:50 +0000

    Adição do form group

--- src/app/core/services/form-busca.service.ts
+++ src/app/core/services/form-busca.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormBuscaService {
 
-  constructor() { }
+  formBusca: FormGroup
+
+  constructor() {
+    this.formBusca = new FormGroup({
+      
+    })
+  }
 }
