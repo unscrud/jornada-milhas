c4f11cace44e02ae0f0bd37968fcf6ec36dde1e8
unscrud <danieldevop@gmail.com>
2024-09-14 11:05:27 +0000

    Criação do componente dropdown-uf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,6 +30,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { PromocoesComponent } from './pages/home/promocoes/promocoes.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { DropdownUfComponent } from './shared/form-busca/dropdown-uf/dropdown-uf.component';
 
 @NgModule({
   declarations: [
@@ -45,7 +46,8 @@ import {MatAutocompleteModule} from '@angular/material/autocomplete';
     FormBuscaComponent,
     ModalComponent,
     SelecaoIdadeComponent,
-    PromocoesComponent
+    PromocoesComponent,
+    DropdownUfComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/form-busca/dropdown-uf/dropdown-uf.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dropdown-uf',
+  templateUrl: './dropdown-uf.component.html',
+  styleUrls: ['./dropdown-uf.component.scss']
+})
+export class DropdownUfComponent {
+
+}
