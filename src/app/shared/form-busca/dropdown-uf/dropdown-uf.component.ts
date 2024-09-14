3555499efd263f60619881306607b548614e2ca0
unscrud <danieldevop@gmail.com>
2024-09-14 11:38:59 +0000

    Recebendo label e icone do componente pai

--- src/app/shared/form-busca/dropdown-uf/dropdown-uf.component.ts
+++ src/app/shared/form-busca/dropdown-uf/dropdown-uf.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dropdown-uf',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./dropdown-uf.component.scss']
 })
 export class DropdownUfComponent {
-
+  @Input() label: string = ""
+  @Input() iconePrefixo: string = ""
 }
