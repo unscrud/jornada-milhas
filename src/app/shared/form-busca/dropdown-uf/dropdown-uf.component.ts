69e3db6414592397f5f2574c760ad3e8d176bb32
unscrud <danieldevop@gmail.com>
2024-09-14 12:28:57 +0000

    Buscando estados para preencher o dropdown

--- src/app/shared/form-busca/dropdown-uf/dropdown-uf.component.ts
+++ src/app/shared/form-busca/dropdown-uf/dropdown-uf.component.ts
@@ -1,11 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { UnidadeFederativaService } from 'src/app/core/services/unidade-federativa.service';
+import { UnidadeFederativa } from 'src/app/core/types/unidade-federativa';
 
 @Component({
   selector: 'app-dropdown-uf',
   templateUrl: './dropdown-uf.component.html',
   styleUrls: ['./dropdown-uf.component.scss']
 })
-export class DropdownUfComponent {
+export class DropdownUfComponent implements OnInit {
   @Input() label: string = ""
   @Input() iconePrefixo: string = ""
+
+  unidadesFederativas: UnidadeFederativa[] = []
+
+  filteredOptions = []
+
+  constructor( private ufService: UnidadeFederativaService) {}
+
+  ngOnInit(): void {
+    this.ufService.listar()
+        .subscribe(dados => {
+          this.unidadesFederativas = dados
+        })
+  }
 }
