a33f3fa7407edf75d54ce33360a20ba9a019b844
unscrud <danieldevop@gmail.com>
2024-09-15 17:17:02 +0000

    Buscando depoimentos

--- src/app/pages/home/depoimentos/depoimentos.component.ts
+++ src/app/pages/home/depoimentos/depoimentos.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DepoimentoService } from 'src/app/core/services/depoimento.service';
 
 @Component({
   selector: 'app-depoimentos',
   templateUrl: './depoimentos.component.html',
   styleUrls: ['./depoimentos.component.scss']
 })
-export class DepoimentosComponent {
+export class DepoimentosComponent implements OnInit {
+
+  constructor(private servico: DepoimentoService){}
+
+  ngOnInit(): void {
+    this.servico.listar()
+        .subscribe( depoimentos => {
+          console.log(depoimentos)
+        })
+  }
+
 
 }
