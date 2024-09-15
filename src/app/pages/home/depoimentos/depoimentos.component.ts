0f1d706568f7771ac9e046740fb6f4828a82f803
unscrud <danieldevop@gmail.com>
2024-09-15 17:18:36 +0000

    Passando os depoimentos recebidos para o atributo depoimentos

--- src/app/pages/home/depoimentos/depoimentos.component.ts
+++ src/app/pages/home/depoimentos/depoimentos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DepoimentoService } from 'src/app/core/services/depoimento.service';
+import { Depoimento } from 'src/app/core/types/depoimento';
 
 @Component({
   selector: 'app-depoimentos',
@@ -7,13 +8,14 @@ import { DepoimentoService } from 'src/app/core/services/depoimento.service';
   styleUrls: ['./depoimentos.component.scss']
 })
 export class DepoimentosComponent implements OnInit {
+  depoimentos!: Depoimento[]
 
   constructor(private servico: DepoimentoService){}
 
   ngOnInit(): void {
     this.servico.listar()
         .subscribe( depoimentos => {
-          console.log(depoimentos)
+          this.depoimentos = depoimentos
         })
   }
 
