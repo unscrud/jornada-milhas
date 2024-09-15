c4c89f454d2e1d62fe779d272ca724fcb11af8af
unscrud <danieldevop@gmail.com>
2024-09-15 14:28:18 +0000

    Criação do componente depoimentos

---
+++ src/app/pages/home/depoimentos/depoimentos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-depoimentos',
+  templateUrl: './depoimentos.component.html',
+  styleUrls: ['./depoimentos.component.scss']
+})
+export class DepoimentosComponent {
+
+}
