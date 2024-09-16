7d466d46270d3bd1510760708ab38d81db18eed3
unscrud <danieldevop@gmail.com>
2024-09-16 18:01:14 +0000

    Passando controle para o componente

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -8,9 +8,9 @@ import { FormBuscaService } from 'src/app/core/services/form-busca.service';
 })
 export class ModalComponent {
   itens = [
-    { titulo: 'Adultos', descricao: '(Acima de 12 anos)' },
-    { titulo: 'Crianças', descricao: '(Entre 2 e 11 anos)' },
-    { titulo: 'Bebês', descricao: '(Até 2 anos)' }
+    { titulo: 'Adultos', descricao: '(Acima de 12 anos)', controle: this.formBuscaService.obterControle('adultos')},
+    { titulo: 'Crianças', descricao: '(Entre 2 e 11 anos)', controle: this.formBuscaService.obterControle('criancas')},
+    { titulo: 'Bebês', descricao: '(Até 2 anos)', controle: this.formBuscaService.obterControle('bebes')}
   ];
 
   constructor(public formBuscaService: FormBuscaService) {}
