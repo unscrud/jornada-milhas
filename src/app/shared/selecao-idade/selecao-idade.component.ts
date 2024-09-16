e048e7c4e10fe07100c158c22a7dcb407299346b
unscrud <danieldevop@gmail.com>
2024-09-16 16:14:23 +0000

    Implementando o ControlValueAcessor e seus métodos necessários

--- src/app/shared/selecao-idade/selecao-idade.component.ts
+++ src/app/shared/selecao-idade/selecao-idade.component.ts
@@ -1,11 +1,32 @@
 import { Component, Input } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
 
 @Component({
-  selector: 'app-selecao-idade',
-  templateUrl: './selecao-idade.component.html',
-  styleUrls: ['./selecao-idade.component.scss']
+  selector: "app-selecao-idade",
+  templateUrl: "./selecao-idade.component.html",
+  styleUrls: ["./selecao-idade.component.scss"],
 })
-export class SelecaoIdadeComponent {
-  @Input() titulo: string = ''
-  @Input() descricao: string = ''
+export class SelecaoIdadeComponent implements ControlValueAccessor {
+  @Input() titulo: string = "";
+  @Input() descricao: string = "";
+
+  value: number = 0;
+  onChange = () => {};
+  onTouch = () => {};
+
+  writeValue(val: any): void {
+    this.value = val
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    throw new Error("Method not implemented.");
+  }
 }
