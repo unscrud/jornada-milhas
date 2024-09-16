110786f7a5f380fb2ba1fd46709069f18adaef46
unscrud <danieldevop@gmail.com>
2024-09-16 01:25:11 +0000

    adição de alterarTipo() e de ponto e vírgulas

--- src/app/core/services/form-busca.service.ts
+++ src/app/core/services/form-busca.service.ts
@@ -1,36 +1,43 @@
 import { Injectable } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { MatChipSelectionChange } from '@angular/material/chips';
 import { MatDialog } from '@angular/material/dialog';
 import { ModalComponent } from 'src/app/shared/modal/modal.component';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class FormBuscaService {
-
-  formBusca: FormGroup
+  formBusca: FormGroup;
 
   constructor(private dialog: MatDialog) {
     this.formBusca = new FormGroup({
       somenteIda: new FormControl(false),
       origem: new FormControl(null),
       destino: new FormControl(null),
-      tipo: new FormControl("Econômica")
-    })
+      tipo: new FormControl("Econômica"),
+    });
   }
 
-  obterControle(nome:string): FormControl {
+  obterControle(nome: string): FormControl {
     const control = this.formBusca.get(nome);
 
-    if (!control){
-      throw new Error(`FormControl com nome "${nome}" não existe.`)
+    if (!control) {
+      throw new Error(`FormControl com nome "${nome}" não existe.`);
     }
 
-    return control as FormControl
+    return control as FormControl;
   }
 
-    openDialog() {
-    this.dialog.open(ModalComponent)
+  openDialog() {
+    this.dialog.open(ModalComponent);
+  }
+
+  alterarTipo(evento: MatChipSelectionChange, tipo: string) {
+    if (evento.selected){
+      this.formBusca.patchValue({tipo})
+    }
+    console.log('tipo alterado para: ', tipo)
   }
 
 }
