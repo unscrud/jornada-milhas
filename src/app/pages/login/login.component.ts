0e613c8ba8320d49aa118bc521b7da5449fd3695
unscrud <danieldevop@gmail.com>
2024-09-22 00:57:19 +0000

    Vículo entre a view e o componente

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup
 
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: [null],
+      senha: [null]
+    })
+  }
+
+  login(){
+    console.log('Efetua login com os dados... ', this.loginForm.value)
+  }
 }
