71b11c98fd70c9038d89d3f7eafe1dfdce87c29c
unscrud <danieldevop@gmail.com>
2024-10-19 12:01:27 +0000

    FormGroup

--- src/app/shared/form-base/form-base.component.ts
+++ src/app/shared/form-base/form-base.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form-base',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-base.component.scss']
 })
 export class FormBaseComponent {
-
+  cadastroForm!: FormGroup
 }
