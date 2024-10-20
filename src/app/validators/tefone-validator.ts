5995352bed9b8d23c5d1e620a1f9b3a91bfc62b4
unscrud <danieldevop@gmail.com>
2024-10-20 19:55:39 +0000

    Valida se tefene está completo

---
+++ src/app/validators/tefone-validator.ts
@@ -0,0 +1,21 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+export function telefoneValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+        const telefone = control.value
+
+        if (!telefone) {
+            return null // não valida campo vazio
+        }
+
+        // Remove caracteres não numéricos
+        const telefoneClean: string = telefone.replace(/\D/g, '')
+
+        // Verifica se tem 10 (fixo) ou 11 (celular) dígitos
+        if (telefoneClean.length === 10 
+                || telefoneClean.length === 11)
+            return null // telefone válido
+
+        return {telefoneInvalido: true}
+    }
+}
