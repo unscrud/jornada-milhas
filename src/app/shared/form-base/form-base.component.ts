be5ecb8dedd47e7c5a781179406b64eaed135eaa
unscrud <danieldevop@gmail.com>
2024-10-22 22:01:41 +0000

    Adição do campo estado

--- src/app/shared/form-base/form-base.component.ts
+++ src/app/shared/form-base/form-base.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { telefoneValidator } from 'src/app/validators/tefone-validator';
 import { cpfValidator } from 'src/app/validators/cpf-validator';
+import { UnidadeFederativa } from 'src/app/core/types/unidade-federativa'
 
 @Component({
   selector: 'app-form-base',
@@ -10,6 +11,7 @@ import { cpfValidator } from 'src/app/validators/cpf-validator';
 })
 export class FormBaseComponent implements OnInit {
   cadastroForm!: FormGroup
+  estadoControl = new FormControl<UnidadeFederativa | null>(null, Validators.required)
 
   constructor(
     private formBuilder: FormBuilder
@@ -29,7 +31,7 @@ export class FormBaseComponent implements OnInit {
       cidade: [null, Validators.required],
       genero: ["outro"],
       telefone: [null, [Validators.required, telefoneValidator()]],
-      estado: [null, Validators.required],
+      estado: this.estadoControl,
       email: [null, [Validators.required, Validators.email]],
       senha: [null, [Validators.required, Validators.minLength(8)]],
       confirmarEmail: [null, [Validators.required, Validators.email]],
