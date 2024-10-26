4c446900220c9f84d0b186ceba4b3e479efe7398
unscrud <danieldevop@gmail.com>
2024-10-26 22:57:23 +0000

    Cadastrar usuário

--- src/app/core/services/autenticacao.service.ts
+++ src/app/core/services/autenticacao.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Usuario } from '../types/usuario';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,9 @@ export class AutenticacaoService {
 
   autenticar(email: string, senha: string): Observable<any> {
     return this.http.post(`${this.apiUrl}/auth/login`,{email,senha})
+  }
+
+  cadastrar(usuario: Usuario): Observable<any> {
+    return this.http.post(`${this.apiUrl}/auth/cadastro`,{usuario})
   }
 }
