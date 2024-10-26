3ee4e87225ea89ca014e0f07ef87e1cff6643f30
unscrud <danieldevop@gmail.com>
2024-10-26 23:13:41 +0000

    Diretiva para usar máscara junto com o datepicker

--- src/app/shared/form-base/date-mask.directive.ts
+++ src/app/shared/form-base/date-mask.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appDateMask]'
 })
 export class DateMaskDirective {
+  constructor(private el: ElementRef) {}
 
-  constructor() { }
+  @HostListener('input', ['$event'])
+  onInputChange(event: Event): void {
+    const input = this.el.nativeElement;
+    let trimmed = input.value.replace(/[^0-9]/g, '');
+    
+    if (trimmed.length > 2) {
+      trimmed = trimmed.slice(0, 2) + '/' + trimmed.slice(2);
+    }
+    if (trimmed.length > 5) {
+      trimmed = trimmed.slice(0, 5) + '/' + trimmed.slice(5, 9);
+    }
 
+    input.value = trimmed;
+  }
 }
