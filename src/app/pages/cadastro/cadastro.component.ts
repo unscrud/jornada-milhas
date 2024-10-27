3f587837cc01613e26e4d49f61439a47830df0ce
unscrud <danieldevop@gmail.com>
2024-10-27 11:33:49 +0000

    Realiza cadastro de usuário

--- src/app/pages/cadastro/cadastro.component.ts
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { AutenticacaoService } from 'src/app/core/services/autenticacao.service';
 import { FormularioService } from 'src/app/core/services/formulario.service';
+import { Usuario } from 'src/app/core/types/usuario';
 
 @Component({
   selector: 'app-cadastro',
@@ -9,9 +11,26 @@ import { FormularioService } from 'src/app/core/services/formulario.service';
 export class CadastroComponent {
   perfilComponent: boolean = false
 
-  constructor(private formularioService: FormularioService){}
+  constructor(
+    private formularioService: FormularioService,
+    private authService: AutenticacaoService
+  ){}
 
   cadastrar() {
     const formCadastro = this.formularioService.getCadastro()
+
+    if(formCadastro?.valid) {
+      const usuario = formCadastro.getRawValue() as Usuario
+      this.authService.cadastrar(usuario).subscribe({
+        next: (value) => {
+          console.log("Cadastro realizado com sucesso: ", value)
+        },
+        error: (err) => {
+          console.log("Erro ao realizar cadastro: ", err)
+        }
+
+      })
+    }
+    
   }
 }
