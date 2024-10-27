f21bc07eddeb19506de02533ee88a6a96a92d90c
unscrud <danieldevop@gmail.com>
2024-10-27 19:04:19 +0000

    Validator que compara o valor de dois campos

---
+++ src/app/validators/compara-campos-validator.ts
@@ -0,0 +1,18 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+export function equalTo(otherField: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+        const fieldValue = control.value
+        const otherFieldValue = control.root.get(otherField)?.value
+
+        if (!fieldValue) {
+            return null // não valida campo vazio
+        }
+
+        if (fieldValue !== otherFieldValue) {
+            return {equalTo: true}
+        }
+
+        return null
+    }
+}
