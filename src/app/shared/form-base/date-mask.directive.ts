3d93e138ffad4651d6c09bb64479c51d95fe7c6f
unscrud <danieldevop@gmail.com>
2024-10-27 18:16:56 +0000

    ainda não funciona, reavaliar

--- src/app/shared/form-base/date-mask.directive.ts
+++ src/app/shared/form-base/date-mask.directive.ts
@@ -8,16 +8,17 @@ export class DateMaskDirective {
 
   @HostListener('input', ['$event'])
   onInputChange(event: Event): void {
-    const input = this.el.nativeElement;
-    let trimmed = input.value.replace(/[^0-9]/g, '');
-    
-    if (trimmed.length > 2) {
-      trimmed = trimmed.slice(0, 2) + '/' + trimmed.slice(2);
-    }
-    if (trimmed.length > 5) {
-      trimmed = trimmed.slice(0, 5) + '/' + trimmed.slice(5, 9);
-    }
-
-    input.value = trimmed;
+    // diretiva ainda não está funcionando adequadamente, reconsiderar
+    //const input = this.el.nativeElement;
+    //let trimmed = input.value.replace(/[^0-9]/g, '');
+    //
+    //if (trimmed.length > 2) {
+    //  trimmed = trimmed.slice(0, 2) + '/' + trimmed.slice(2);
+    //}
+    //if (trimmed.length > 5) {
+    //  trimmed = trimmed.slice(0, 5) + '/' + trimmed.slice(5, 9);
+    //}
+//
+    //input.value = trimmed;
   }
 }
