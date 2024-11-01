7babeb004690e8f98f533ded3447f4d2cbf82d43
unscrud <danieldevop@gmail.com>
2024-11-01 01:15:27 +0000

    Injeta dependências e inicializa props

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AutenticacaoService } from 'src/app/core/services/autenticacao.service';
+import { TokenService } from 'src/app/core/services/token.service';
+import { Usuario } from 'src/app/core/types/usuario';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
   styleUrls: ['./perfil.component.scss']
 })
-export class PerfilComponent {
+export class PerfilComponent implements OnInit {
   titulo: string = 'Olá Pessoa'
   textoBotao: string = 'ATUALIZAR'
   perfilComponent: boolean = true
+
+  token: string = ''
+  nome: string = ''
+  cadastro!: Usuario
+
+  constructor(
+    private tokenService: TokenService,
+    private authService: AutenticacaoService
+  ){}
+
+  ngOnInit(): void {
+    this.token = this.tokenService.retornarToken()
+    this.authService.buscarCadastro(this.token).subscribe(cadastro =>{
+      this.cadastro = cadastro
+      this.nome = this.cadastro.nome
+    })
+  }
 
   deslogar(){
     console.log('SAIR.... ')
