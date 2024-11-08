887bc978137b70109ec6d86dabb528ed401c7487
unscrud <danieldevop@gmail.com>
2024-11-08 13:33:28 +0000

    Vai para busca na ocorrência do evento

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {}
+
+export class HomeComponent {
+  constructor(private router: Router){}
+
+  navegarParaBusca(event: any){
+    this.router.navigate(['/busca'])
+  }
+}
