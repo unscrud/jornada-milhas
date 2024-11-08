1ba398f8d8d02fa7f012088287b1dbb3b74f1fce
unscrud <danieldevop@gmail.com>
2024-11-08 11:18:59 +0000

    Exibe perfil e sair se usuário logado

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsuarioService } from 'src/app/core/services/usuario.service';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  constructor(
+    private userService: UsuarioService,
+    private router: Router
+  ) {}
+
+  user$ = this.userService.returnUser()
+
+  logout(){
+    this.userService.logout()
+    this.router.navigate(['/login'])
+  }
 }
