2ce9fc4707b0574dd9a7e574813657ed2c2fe41d
unscrud <danieldevop@gmail.com>
2024-11-10 18:50:50 +0000

    Componente acionado com uma busca padrão

--- src/app/pages/busca/busca.component.ts
+++ src/app/pages/busca/busca.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PassagensService } from 'src/app/core/services/passagens.service';
 
 @Component({
   selector: 'app-busca',
   templateUrl: './busca.component.html',
   styleUrls: ['./busca.component.scss']
 })
-export class BuscaComponent {
+export class BuscaComponent implements OnInit {
+  constructor(
+    private passagensService: PassagensService,
+  ){}
+
+  ngOnInit(): void {
+    const buscaPadrao = {
+      somenteIda: false,
+      passageirosAdultos: 1,
+      passageirosCriancas: 0,
+      passageirosBebes: 0,
+      tipo: "Executiva",
+      dataIda: new Date().toISOString(),
+      pagina: 1,
+      porPagina: 25
+    }
+
+    this.passagensService.getPassagens(buscaPadrao).subscribe(res => console.log(res))
+  }
 
 }
