cd378229abaae625a875a6da8d95a75ea69b02fb
unscrud <danieldevop@gmail.com>
2024-11-14 14:26:24 +0000

    Desabilita data de volta se somente ida estiver selecionado

--- src/app/core/services/form-busca.service.ts
+++ src/app/core/services/form-busca.service.ts
@@ -11,8 +11,11 @@ export class FormBuscaService {
   formBusca: FormGroup;
 
   constructor(private dialog: MatDialog) {
+    const somenteIda = new FormControl(false, Validators.required)
+    const dataVolta = new FormControl(null, Validators.required)
+
     this.formBusca = new FormGroup({
-      somenteIda: new FormControl(false, Validators.required),
+      somenteIda,
       origem: new FormControl(null, Validators.required),
       destino: new FormControl(null, Validators.required),
       tipo: new FormControl("Econômica"),
@@ -20,8 +23,19 @@ export class FormBuscaService {
       criancas: new FormControl(0),
       bebes: new FormControl(0),
       dataIda: new FormControl(null, Validators.required),
-      dataVolta: new FormControl(null, Validators.required),
+      dataVolta,
     });
+
+    somenteIda.valueChanges.subscribe(somenteIda => {
+      if(somenteIda){
+        dataVolta.disable()
+        dataVolta.setValidators(null)
+      } else {
+        dataVolta.enable()
+        dataVolta.setValidators(Validators.required)
+      }
+      dataVolta.updateValueAndValidity()
+    })
   }
 
   getDescricaoDePassageiros(): string {
