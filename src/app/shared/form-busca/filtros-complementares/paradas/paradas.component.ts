799476ee270e15c9c3807e32a72e8e286d53c142
unscrud <danieldevop@gmail.com>
2024-11-16 17:16:14 +0000

    Manipula conexões

--- src/app/shared/form-busca/filtros-complementares/paradas/paradas.component.ts
+++ src/app/shared/form-busca/filtros-complementares/paradas/paradas.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { FormBuscaService } from 'src/app/core/services/form-busca.service';
+import { OpcoesDeParada } from 'src/app/core/types/opcoes-de-parada';
 
 @Component({
   selector: 'app-paradas',
   templateUrl: './paradas.component.html',
   styleUrls: ['./paradas.component.scss']
 })
-export class ParadasComponent {
-  opcoes = [
+export class ParadasComponent implements OnInit {
+  opcoesSelecionadas: OpcoesDeParada | null = null  
+
+  opcoes: OpcoesDeParada[] = [
     {
       display: 'Direto',
       value: '0',
@@ -24,4 +29,22 @@ export class ParadasComponent {
       value: '3',
     },
   ]
+
+  conexoesControl: FormControl<number | null>
+
+  constructor(
+    private formBuscaService: FormBuscaService,
+  ) {
+    this.conexoesControl = this.formBuscaService.obterControle<number>('conexoes')
+  }
+
+  ngOnInit(): void {
+    this.conexoesControl.valueChanges.subscribe(
+      (value) => {
+        if(!value){
+          this.opcoesSelecionadas = null
+        }
+      }
+    )
+  }
 }
