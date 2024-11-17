cdd5947b1081ec1d5561b272c95f847fb281946e
unscrud <danieldevop@gmail.com>
2024-11-17 18:01:16 +0000

    Conversor para transformar params em string para atender a api

--- src/app/core/services/passagens.service.ts
+++ src/app/core/services/passagens.service.ts
@@ -1,9 +1,9 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { Passagem } from '../types/passagem';
 import { Resultado } from '../types/resultado';
 import { Observable } from 'rxjs';
+import { DadosBusca } from '../types/dados-busca';
 
 @Injectable({
   providedIn: 'root'
@@ -13,9 +13,23 @@ export class PassagensService {
 
   constructor(private httpClient: HttpClient){}
 
-  getPassagens(search: any) : Observable<Resultado>{
-    const params = search
+  getPassagens(search: DadosBusca) : Observable<Resultado>{
+    const params = this.converterParametrosParaString(search)
     
-    return this.httpClient.get<Resultado>(`${this.apiUrl}/passagem/search`, {params} )
+    return this.httpClient
+        .get<Resultado>(`${this.apiUrl}/passagem/search${params}`)
+  }
+
+  converterParametrosParaString(busca: DadosBusca){
+    const query = Object.entries(busca)
+        .map( ([key,value]) => {
+          if(!value){
+            return '' 
+          }
+          return `${key}=${value}`
+        })
+        .join('&')
+
+    return query
   }
 }
