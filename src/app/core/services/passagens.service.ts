f2eefc76a0d1932f7da749ecc1f4ab7512936af8
unscrud <danieldevop@gmail.com>
2024-11-19 16:19:31 +0000

    Atribui preço mínimo e máximo

--- src/app/core/services/passagens.service.ts
+++ src/app/core/services/passagens.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Resultado } from '../types/resultado';
-import { Observable } from 'rxjs';
+import { Observable, take } from 'rxjs';
 import { DadosBusca } from '../types/dados-busca';
 
 @Injectable({
@@ -10,14 +10,22 @@ import { DadosBusca } from '../types/dados-busca';
 })
 export class PassagensService {
   apiUrl: string = environment.apiUrl2
+  precoMin: number = 0
+  precoMax: number = 0
 
   constructor(private httpClient: HttpClient){}
 
   getPassagens(search: DadosBusca) : Observable<Resultado>{
     const params = this.converterParametrosParaString(search)
-    
-    return this.httpClient
+    const obs = this.httpClient
         .get<Resultado>(`${this.apiUrl}/passagem/search?${params}`)
+
+    obs.pipe(take(1)).subscribe(res => {
+      this.precoMin = res.precoMin
+      this.precoMax = res.precoMax
+    })
+
+    return obs
   }
 
   converterParametrosParaString(busca: DadosBusca){
