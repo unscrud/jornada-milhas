5e9be0fc2c5a424e767d8322ca4b7fbf514d2ee0
unscrud <danieldevop@gmail.com>
2024-11-19 16:20:50 +0000

    Se valor nulo, as opções também serão nulas

--- src/app/shared/form-busca/filtros-complementares/paradas/paradas.component.ts
+++ src/app/shared/form-busca/filtros-complementares/paradas/paradas.component.ts
@@ -41,7 +41,7 @@ export class ParadasComponent implements OnInit {
   ngOnInit(): void {
     this.conexoesControl.valueChanges.subscribe(
       (value) => {
-        if(!value){
+        if(value === null){
           this.opcoesSelecionadas = null
         }
       }
